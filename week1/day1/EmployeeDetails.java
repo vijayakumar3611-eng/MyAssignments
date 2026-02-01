611a051a205498c50622fbdea1ed795f3a245d8a
vijayakumar3611-eng <vijayakumar3611@gmail.com>
2026-02-01 09:22:56 +0530

    Add files via upload

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		byte age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		short pinCode = (short) 56001;
+		char luckyNumber = '7';
+		System.out.println("Employee Name;"+ name + "\n" + "Employee Age;"+ age + "\n" + "Employee Salary;"+ salary + "\n" + "Employee Gender;"+ gender + "\n" + "Accept Terms;"+ acceptTerms + "\n" + "Empyoyee Rating;"+ rating + "\n" + "Employee Mobile Number;" + mobileNumber + "\n" + "Pincode" + pinCode + "\n" + "Employee Lucky Number;" + luckyNumber + "\n");
+	}
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion=100.2f;
+String browserName="firefox";
+ boolean isVisible=true;
+ int releaseyear=1998;
+ char browserLogo='f';
+ System.out.println("Browser version is:" +browserVersion+ "\n" + "The BrowserName is:" + browserName + "\n" + "The BrowserName is:\"Browser Is Visible:" + isVisible + "\n" + "Relase version:" + releaseyear + "\n" + "BrowserLogo:" + browserLogo);
+	}
+
+}
